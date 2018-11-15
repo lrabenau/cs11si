871cb68f0893586377c57906ff70ee46419be6a2
Lisa <l.von.rabenau@gmail.com>
2018-11-15 14:29:19 -0800

    added bird world

---
+++ Fishbowl/Assets/ControllerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // #if UNITY_EDITOR
+        // #else if bUNITY_ANDROID
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryHandTrigger) || Input.GetKeyDown(KeyCode.Space))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.forward, out hit, 100.0f))
+            {
+                if (hit.distance <= 10f)
+                {
+                    if (hit.collider.gameObject.tag == "sceneSwitcher"){ 
+                        // if (tag == x)
+                        // sceneName=y
+                        GameManager.Instance.LoadScene("Scenes/SampleScene");
+
+                    }
+                }
+            }
+        }
+	}
+}
