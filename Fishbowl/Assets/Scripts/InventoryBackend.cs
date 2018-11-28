b0c283c4549c493996840d839398a127ab883429
Alex <alee899@stanford.edu>
2018-11-27 17:46:05 -0800

    Finished inventory

---
+++ Fishbowl/Assets/Scripts/InventoryBackend.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+
+
+public class InventoryBackend : MonoBehaviour {
+    public List<string> inventory = new List<string>();  
+
+    public void addItem(string newItem)
+    {
+        inventory.Add(newItem); 
+    }
+
+    public bool containsItem(string item)
+    {
+        return inventory.Contains(item); 
+    }
+
+    public bool removeItem(string item)
+    {
+        return inventory.Remove(item); 
+    }
+
+}
