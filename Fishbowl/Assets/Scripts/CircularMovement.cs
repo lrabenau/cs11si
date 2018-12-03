f65bd9a9ade01c63670cef6341a95188caffec78
Lisa <l.von.rabenau@gmail.com>
2018-12-02 22:33:57 -0800

    pushing it again

---
+++ Fishbowl/Assets/Scripts/CircularMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircularMovement : MonoBehaviour
+{
+    public Vector3 Velocity = new Vector3(0, 0, 0);
+    public Vector3 CenterStart = new Vector3(0, 0, 0);
+
+    [Range(0, 100)] 
+    public float RotateSpeed = 1f;
+    [Range(0, 100)]
+    public float RotateRadiusX = 1f;
+    [Range(0, 100)]
+    public float RotateRadiusZ = 1f;
+
+    public bool Clockwise = true;
+
+    private Vector3 _centre;
+    private float _angle;
+
+    private void Start()
+    {
+//%  _centre = transform.position;
+        _centre = CenterStart;
+    }
+
+    private void Update()
+    {
+        _centre += Velocity * Time.deltaTime;
+
+        _angle += (Clockwise ? RotateSpeed : -RotateSpeed) * Time.deltaTime;
+
+        var x = Mathf.Sin(_angle) * RotateRadiusX;
+        var z = Mathf.Cos(_angle) * RotateRadiusZ;
+
+        transform.position = _centre + new Vector3(x, 0, z);
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(_centre, 0.1f);
+        Gizmos.DrawLine(_centre, transform.position);
+    }
+}
---
+++ Fishbowl/Assets/Scripts/ControllerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // #if UNITY_EDITOR
+        // #else if bUNITY_ANDROID
+        //if (OVRInput.GetDown(OVRInput.Button.PrimaryHandTrigger) || Input.GetKeyDown(KeyCode.Space)) {
+            RaycastHit hit;
+           
+            if (Physics.Raycast(transform.position, transform.up, out hit, 10.0f))
+            {
+                Debug.Log("HIT");
+                Debug.Log(hit.collider.gameObject.tag);
+                if (hit.distance <= 5f)
+                {
+                    if (hit.collider.gameObject.tag == "spaceScene"){ 
+                        // if (tag == x)
+                        // sceneName=y
+                        GameManager.Instance.LoadScene("Scenes/space");
+
+                    }
+                    if (hit.collider.gameObject.tag == "birdScene")
+                    {
+
+                        GameManager.Instance.LoadScene("Scenes/Snow_birds");
+
+                    }
+                    if (hit.collider.gameObject.tag == "fishbowlScene")
+                    {
+
+                        GameManager.Instance.LoadScene("Scenes/Fishbowl");
+
+                    }
+                }
+            }
+        //}
+	}
+}
