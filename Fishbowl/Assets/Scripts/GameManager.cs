5926687e264e37b399158ba52187621415f7de6c
Alex <alee899@stanford.edu>
2018-12-04 12:51:31 -0800

    Updated gamemanager for one line

--- Fishbowl/Assets/Scripts/GameManager.cs
+++ Fishbowl/Assets/Scripts/GameManager.cs
@@ -24,6 +24,10 @@ public class GameManager : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
+        if (Inventory.inventory.Count >= 2)
+        {
+            SceneManager.LoadScreen("Scenes/Fishbowl")
+        }
         //RaycastHit hit; 
         //if (Physics.Raycast(transform.position, -Vector3.up, out hit, 100.0f)) {
         //    if (hit.distance <= 10f) {
